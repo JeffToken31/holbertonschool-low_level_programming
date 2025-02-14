a90a839705ad1f0b58a1a407467906b788a2f1f5
jeff <jeffrey-31@hotmail.fr>
2025-02-14 17:10:16 +0100

    putchar function

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - putchar function
+ *Return: 0 if correct
+ */
+int main(void)
+{
+	int ch = 97;
+
+	for (;ch < 123; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
