8a3485455751b79da7c72e5e359e56be5804ad3a
jeff <jeffrey-31@hotmail.fr>
2025-02-14 10:51:37 +0100

    My firts program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/*Testing puts funtion*/
+/**
+ *main - puts function
+ *Return: 0 if correct
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
