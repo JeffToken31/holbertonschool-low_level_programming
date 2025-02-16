bdcaee4ff8a64fe561b67375407cf2bccdc86aa2
jeff <jeffrey-31@hotmail.fr>
2025-02-16 18:18:46 +0100

    Manipulation of ASCII table

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - putchar function
+ *Return: 0 if correct
+ */
+int main(void)
+{
+	int minchar = 97;
+	int maxchar = 65;
+
+	for (; minchar < 123; minchar++)
+	{
+	putchar(minchar);
+	}
+	
+	for (; maxchar < 91; maxchar++)
+	{
+	putchar(maxchar);
+	}
+	putchar(36);
+	return (0);
+}
