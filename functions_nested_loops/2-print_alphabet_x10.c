07a8b7082bd4635b1318dd3189d7d81d79985fcc
jeff <jeffrey-31@hotmail.fr>
2025-02-17 13:52:58 +0100

    first nested loop

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet - to print list alphabet
+ *print_alphabet_x10 - to print list alphabet x10
  *
  *Return: (0) if correct
  */
@@ -9,9 +9,10 @@ int print_alphabet_x10(void)
 {
 	int  nbra;
 	int lsa;
+
 	for (nbra = 0; nbra < 10; nbra++)
 	{
-	lsa =97;
+	lsa = 97;
 		for (; lsa < 123; lsa++)
 		{
 		_putchar(lsa);
