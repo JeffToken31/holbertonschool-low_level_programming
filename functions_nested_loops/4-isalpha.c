cb2b9f606e36d69f03be00f4217c13af381cd981
jeff <jeffrey-31@hotmail.fr>
2025-02-17 16:18:21 +0100

    use && and ||in if conditions)

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-*_islower - is to check if islowercase
+*_isalpha - is to check if isalphanumeric
 *@c: arg type int
 *Return:1 is true
 */
 int _isalpha(int c)
 {
-	if (c >= 97 && c < 123) || (c >= 65 && c < 91)
+	if ((c >= 97 && c < 123) || (c >= 65 && c < 91))
 		return (1);
 	else
 		return (0);
