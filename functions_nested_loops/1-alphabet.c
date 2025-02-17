b60fedd0a828a11fcff70bca581ac08dd9a41d38
jeff <jeffrey-31@hotmail.fr>
2025-02-17 12:11:40 +0100

    call void function no need return value

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *main - putchar function
- *Return: 0 if correct
+ *print_alphabet - to print list alphabet
+ *
+ *Return: no return if correct
  */
 void print_alphabet(void)
 {
 	int lsa = 97;
-	
+
 	for (; lsa < 123; lsa++)
 	{
 	_putchar(lsa);
 	}
 	_putchar('\n');
-	return;
 }
