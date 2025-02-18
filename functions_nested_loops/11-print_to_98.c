aa1c5ca38b634f68e4931c0f168b75eb2c697d08
jeff <jeffrey-31@hotmail.fr>
2025-02-18 19:34:08 +0100

    to print still 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - is to all numbers still 98
+ *@n: type int
+ *Return: no need
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98;)
+		{
+			if (n == 98)
+			{
+				printf("%d \n", n);
+				n--;
+			}
+			else
+			{
+				printf("%d, ", n);
+				n--;
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98;)
+		{
+			printf("%d, ", n);
+			n++;
+
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				n++;
+			}
+		}
+	}
+
+}
