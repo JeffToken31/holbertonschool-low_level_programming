94d193722d02275c540e6c46780ac4128a08689f
jeff <jeffrey-31@hotmail.fr>
2025-02-18 13:25:11 +0100

    retype my putchar

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,19 +1,13 @@
-#include <stdio.h>
-#include "_putchar.h"
+#include <unistd.h>
+
 /**
- *main - putchar function
- *Return: 0 if correct
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(void)
+int _putchar(char c)
 {
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
-	putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
