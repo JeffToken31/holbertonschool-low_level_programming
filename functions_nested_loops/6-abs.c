1bb82631bea4cf58c3d7b5ef1f761fd6f5645ddb
jeff <jeffrey-31@hotmail.fr>
2025-02-18 09:18:14 +0100

    to return a absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_abs - is to return absolute value number
+*@n: arg type int
+*Return: n or -n
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
