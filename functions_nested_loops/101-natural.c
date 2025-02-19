bdc39136eef9f29cc2b19d16372a91402cb44d54
jeff <jeffrey-31@hotmail.fr>
2025-02-19 13:47:26 +0100

    print sum of all natural numbers

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_sum - print sum of natural number
+ *@: no need
+ *Return: no need
+ */
+void print_sum(void)
+{
+	int nbrmul3, nbrmul5, sumof3, sumof5, sumall, i;
+
+	for (i = 1024; i > 0;)
+	{
+		if (i % 3 != 0)
+		{
+			i--;
+		}
+		else
+		{
+			nbrmul3 = i;
+			i = 0;
+		}
+	}
+	for (i = 1024; i > 0;)
+	{
+		if (i % 5 != 0)
+		{
+		i--;
+		}
+		else
+		{
+			nbrmul5 = i;
+			i = 0;
+		}
+	}
+	sumof3 = nbrmul3 * (3 + (nbrmul3 * 3)) / 2;
+	sumof5 = nbrmul5 * (5 + (nbrmul5 * 5)) / 2;
+	sumall = sumof3 + sumof5;
+	printf("%d\n", sumall);
+}
