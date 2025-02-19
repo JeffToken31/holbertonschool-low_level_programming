088db02dd8cc4aa51f9ec2d4d244c301b35bb7aa
jeff <jeffrey-31@hotmail.fr>
2025-02-19 21:27:17 +0100

    prints all possible different combinations of two digits

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - putchar function
+ *Return: 0 if correct
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+				putchar(a + '0');
+				putchar(b + '0');
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
