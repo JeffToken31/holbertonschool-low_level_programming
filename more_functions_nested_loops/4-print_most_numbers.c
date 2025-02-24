6da0bd020ce10b7dbe828dca7e42b97248148563
jeff <jeffrey-31@hotmail.fr>
2025-02-24 10:46:02 +0100

    print digit unless 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,22 +2,23 @@
 
 /**
  *print_most_numbers - print digit without 2 and 4
- *Return: 0 if correct
+ *Return: no needed
  */
 void print_most_numbers(void)
 {
 	int digit = 48;
 
-	for (; digit <= 57; digit++)
+	for (; digit <= 57;)
 	{
 		if (digit == 59 || digit == 61)
 		{
+			digit++;
 		}
 		else
 		{
-		putchar(digit);
+			digit++;
+			putchar(digit);
 		}
 	}
 	putchar('\n');
-	return (0);
 }
