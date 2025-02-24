36a936864ee8c5da1a11dc9575cea6a9593e3ab5
jeff <jeffrey-31@hotmail.fr>
2025-02-24 19:11:19 +0100

    to discovery prime numbers and prime factors

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - print largest factor of number
+ *@: no need
+ *Return: 0 if sucess
+ */
+int main(void)
+{
+	long number, divid;
+
+	number = 612852475143;
+	divid = 3;
+
+	for (; divid * divid <= number;)
+	{
+		if (number % divid != 0)
+			divid += 2;
+		else
+		{
+			number /= divid;
+			printf("%ld", number);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
