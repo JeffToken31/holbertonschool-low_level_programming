3ba410a3ce7190350cbfc481d03768b56edd0eeb
jeff <jeffrey-31@hotmail.fr>
2025-02-24 16:05:02 +0100

    to print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_triangle - to draw a triangle
+ *@size: type int
+ *Return: no needed
+ */
+void print_triangle(int size)
+{
+	int line, square, space;
+
+	if (size < 1)
+		_putchar('\n');
+
+	else
+
+	for (line = 1; line <= size; line++)
+	{
+		for (space = size - line; space >= 0; space--)
+		{
+			_putchar(' ');
+		}
+		square = 1;
+		for (; square <= line; square++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
