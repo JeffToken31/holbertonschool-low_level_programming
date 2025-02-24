3ea83304d2597c23543e746dd9a3abdb80f4c70c
jeff <jeffrey-31@hotmail.fr>
2025-02-24 10:34:12 +0100

    print digit's list

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - to print digit's list
+ *
+ *Return: no return if correct
+ */
+void print_numbers(void)
+{
+	int digit = 48;
+
+	for (; digit <= 57; digit++)
+	{
+	_putchar(digit);
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -15,4 +15,5 @@ void print_sum(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 #endif
