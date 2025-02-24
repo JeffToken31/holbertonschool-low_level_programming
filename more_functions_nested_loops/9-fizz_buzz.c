5e32d1b7eefe7d5dfd8597c5d1bb8952792eee15
jeff <jeffrey-31@hotmail.fr>
2025-02-24 15:04:00 +0100

    work with only if condition

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - print still 100 with multiple 3 and 5 = fizz and buzz
+ *@: no need
+ *Return: 0 if sucess
+ */
+int main(void)
+{
+	int digit;
+
+	digit = 0;
+
+	for (; digit < 101; digit++)
+	{
+		if (digit % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		if (digit % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		if (digit % 3 == 0 && digit % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		if (digit % 3 != 0 && digit % 5 != 0)	
+		{
+			printf("%d ",digit);
+		}
+	}
+	putchar('\n');	
+	return (0);
+}
