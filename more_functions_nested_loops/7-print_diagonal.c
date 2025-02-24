5c300f280f3aa8de9cc6f3e4eeaa8ff6bd7da354
jeff <jeffrey-31@hotmail.fr>
2025-02-24 13:19:54 +0100

    to print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_diagonal - to draw a diagonal with spaces and slash
+ *@n: type int
+ *Return: no needed
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (n > 0)
+	{
+		if (n < 0)
+		{
+			_putchar('\n');
+		}
+
+		j = i;
+		while ( j > 0)
+		{
+			_putchar(' ');
+			j--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		n--;
+		i++;
+
+	}
+	_putchar('\n');
+}
