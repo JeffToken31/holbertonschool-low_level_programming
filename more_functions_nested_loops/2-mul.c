4e53620e09de2d1553db926268722ced2e86d02f
jeff <jeffrey-31@hotmail.fr>
2025-02-24 10:24:57 +0100

    to multipli two digits

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *mul - is to multipli two digit
+ *@a: type int
+ *Return: (a * b) if sucess
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -14,4 +14,5 @@ void print_times_table(int n);
 void print_sum(void);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
