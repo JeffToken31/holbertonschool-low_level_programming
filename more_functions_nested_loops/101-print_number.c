3e3df58b9f1ae09962f2aaa4fb9b322acaba3a62
jeff <jeffrey-31@hotmail.fr>
2025-02-24 21:53:59 +0100

    test

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,46 +7,16 @@
  */
 void print_number(int n)
 {
-	if (n < 10 && n > -10)
+	if (n == 0)
+		_putchar(48);
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			n = -n;
-			_putchar('-');
-		}
-			_putchar(n + 48);
+		_putchar('-');
+		n = -n;
 	}
-	else if ((n < 100 && n > 10) || (n > -100 && n < -10))
+	while (n != 0)
 	{
-		if (n < -10)
-		{
-			n = -n;
-			_putchar('-');
-		}
-			_putchar(n / 10 + 48);
-			_putchar(n % 10 + 48);
-	}
-	else if ((n < 1000 && n > 100) || (n > -1000 && n < -100))
-	{
-		if (n < -100)
-		{
-			n = -n;
-			_putchar('-');
-		}
-		_putchar(n / 100 + 48);
-		_putchar((n / 10) % 10 + 48);
-		_putchar(n % 10 + 48);
-	}
-	else
-	{
-		if (n < -1000)
-		{
-			n = -n;
-			_putchar('-');
-		}
-		_putchar(n / 1000 + 48);
-		_putchar((n / 100) % 10 + 48);
-		_putchar((n / 10) % 10 + 48);
-		_putchar(n % 10 + 48);
+		_putchar(n / 10 + 48);
+		n /= 10;
 	}
 }
