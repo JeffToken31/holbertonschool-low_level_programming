cc66ba369bd89af25eb92aadac4032283a15d736
jeff <jeffrey-31@hotmail.fr>
2025-02-25 10:29:37 +0100

    to swap value of two int

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,13 +2,14 @@
 
 /**
  *swap_int - to modifie value of variable with pointer
- *@a, b: pointer type int
+ *@a: pointer type int
+ *@b: pointer type int
  *Return: no needed
  */
 void swap_int(int *a, int *b)
 {
 	int temp;
-	
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
