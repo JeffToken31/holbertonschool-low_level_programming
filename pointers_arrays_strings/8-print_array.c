377520df6df02035de53986c34d1a6b597d4f120
jeff <jeffrey-31@hotmail.fr>
2025-02-25 19:30:31 +0100

    to print elements of array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,13 @@
  */
 void print_array(int *a, int n)
 {
-	int last_n;
-
-	last_n = 0;
-	for (n = 0; a[n]; n++)
-		last_n = n;
 	for (n = 0; a[n];)
 	{
-		if (n != last_n)
-			printf("%d, ", a[n]);
+		if (n != 0)
+			printf(", %d", a[n]);
 		else
-			printf("%d\n", a[n]);
+			printf("%d", a[n]);
 		n++;
 	}
+	printf("\n");
 }
