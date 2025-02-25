c5f2129d4d755c5c6e8664e0b1c01b919b278f93
jeff <jeffrey-31@hotmail.fr>
2025-02-25 12:37:33 +0100

    to reverse a string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_rev - to reverse a string
+ *@s: pointer type char
+ *Return: no needed
+ */
+void print_rev(char *s)
+{
+	int element_of_array;
+
+	for (element_of_array = 0; s[element_of_array];)
+	{
+		element_of_array++;
+	}
+	while (element_of_array >= 0)
+	{
+		_putchar(s[element_of_array]);
+		element_of_array--;
+	}
+	_putchar('\n');
+}
