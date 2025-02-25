eabc73e05686c0ad8e038557b8eb28e77b3082aa
jeff <jeffrey-31@hotmail.fr>
2025-02-25 09:31:02 +0100

    modifie value of variable with pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *reset_to_98 - to modofie value of variable with pointer
- *@*n: pointer type int
+ *reset_to_98 - to modifie value of variable with pointer
+ *@n: pointer type int
  *Return: no needed
  */
 void reset_to_98(int *n)
