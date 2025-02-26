199307c0ab291b104d0ef16524368b2601f72d74
jeff <jeffrey-31@hotmail.fr>
2025-02-26 15:36:39 +0100

    to convert random string to int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,17 +8,25 @@
  */
 int _atoi(char *s)
 {
-	int i, result, count;
+	int i, result, count, first, last;
 	
 	i = 0;
 	result = 0;
 	count = 0;
+	
 	for (; s[i]; i++)
+	{
+		if ((s[i] >= '0' && s[i] <= '9') && (s[i - 1] < '0' || s[i - 1] > '9'))
+			first = i;
+		if ((s[i] >= '0' && s[i] <= '9') && (s[i + 1] < '0' || s[i + 1] > '9'))
+			last = i;
+	}
 
-	if (s[i] >= '0' && s[i] <= '9')
-		result = result * 10 + (s[i] - '0');
-	else if (s[i] == '-')
+	for (i = first; i <= last; i++)
+		result = result * 10 + ((int)s[i] - '0');
+	if (s[i] == '-')
 		count++;
+		
 	if (count % 2 != 0)
 		result *= -1;
 	return (result);
