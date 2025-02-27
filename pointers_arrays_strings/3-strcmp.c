3eefeebd0cc571dd40256784a980d6417310dc03
jeff <jeffrey-31@hotmail.fr>
2025-02-27 19:57:48 +0100

    to count diiference between 1st character 2 variable

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ **_strcmp -to count diiference between 1st character 2 variable
+ *@s1: pointer type char
+ *@s2: pointer type char
+ *Return: value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+
+	for (i = 0; s1[i]; i++)
+	;
+	for (j = 0; s2[j]; j++)
+	;
+
+	if (i < j)
+	{
+		return ((*s1 - i) - (*s2 - j + 1));
+	}
+	else if (i == j)
+	{
+		return (0);
+	}
+	else
+	{
+		return ((*s1 - i + 1) - (*s2 - j));
+	}
+}
