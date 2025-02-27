bcac56b6a62cfae24333e45ffcfe2b356f1710ee
jeff <jeffrey-31@hotmail.fr>
2025-02-27 15:27:21 +0100

    to copy a string in other variable

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0' && j <= n; j++)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[j] = src[j];
 	}
-
+	
+	for (; j < n; j++)
 	dest[j] = '\0';
 
 	return (dest);
