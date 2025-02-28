d560f7aea4058b0ac1b763973e9072334cc13607
jeff <jeffrey-31@hotmail.fr>
2025-02-28 16:41:09 +0100

    to encode 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **leet - to capitalize first letter of word
+ *@s: pointer type char
+ *Return: type char
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char symbole[10] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char digit[10] = { '4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; *(s + i)  != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == symbole[j])
+			{
+				s[i] = digit[j];
+			}
+		}
+	}
+	return (s);
+}
