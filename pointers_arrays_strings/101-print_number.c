bb40af4f62ad78383b11069d6fe5ef17d0504c25
jeff <jeffrey-31@hotmail.fr>
2025-02-28 21:43:18 +0100

    to print number with _putchar

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_number - to print all array's elements
+ *@n: type int
+ *Return: no needed
+ */
+void print_number(int n)
+{
+	int i;
+	int divis = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	i = n;
+	while (i / 10)
+	{
+		i /= 10;
+		divis *= 10;
+	}
+	while (divis > 0)
+	{
+		_putchar('0' + (n / divis) % 10);
+		divis /= 10;
+	}
+}
