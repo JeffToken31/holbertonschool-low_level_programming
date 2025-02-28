ce2e0ef7a57fc42659d8c47c6e4fb80c1fb8d5cd
jeff <jeffrey-31@hotmail.fr>
2025-02-28 21:57:04 +0100

    to print number with _putchar

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
+#define INT_MIN (-2147483648)
 /**
  *print_number - to print number with _putchar
  *@n: type int
@@ -11,7 +12,9 @@ void print_number(int n)
 	int i;
 	int divis = 1;
 
-	if (n < 0)
+	if (n == INT_MIN)
+		n = -n / 10;
+	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
@@ -28,4 +31,6 @@ void print_number(int n)
 		_putchar('0' + (n / divis) % 10);
 		divis /= 10;
 	}
+	if (n == INT_MIN)
+		_putchar('8');
 }
