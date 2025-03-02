dd50380fcbf37be2e651836c0c29cc90dca28031
jeff <jeffrey-31@hotmail.fr>
2025-03-02 22:34:55 +0100

    fix overflow, to cast variable in other type temporaly

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #define INT_MIN (-2147483648)
+#define INT_MAX (-2147483647)
 
 /**
  **_atoi - to convert a char in int
@@ -9,35 +10,37 @@
  */
 int _atoi(char *s)
 {
-	int i, result, count, overflow;
+	int i, is_minus;
+	unsigned int result;
 
 	i = 0;
 	result = 0;
-	count = 0;
+	is_minus = 1;
 
-	for (; s[i]; i++)
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			overflow = result;
-			result = result * 10 + ((int)s[i] - '0');
-			if (result / 10 != overflow)
-				result = INT_MIN;
-		}
-		else if (s[i] == '-')
-		{
-			count++;
-		}
-		else if (result != 0)
-		{
-			break;
-		}
-		else if (result == INT_MIN)
-		{
-			return (result);
-		}
+		if (s[i] == '-')
+			is_minus *= -1;
+
+		i++;
 	}
-	if (count % 2 != 0)
-		result *= -1;
-	return (result);
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+
+		result = result * 10 + (s[i] - '0');
+		
+		if (is_minus == 1 && result > (unsigned)INT_MAX)
+		{
+			return (INT_MAX);
+		}
+		if (is_minus == -1 && result > (unsigned)INT_MAX + 1)
+		{
+			return (INT_MIN);
+		}
+
+		i++;
+	}
+
+	return ((int)result * is_minus);
 }
