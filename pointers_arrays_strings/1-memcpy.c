493a4dc79e01a03094c6dfc627dc006dc9967ffa
jeff <jeffrey-31@hotmail.fr>
2025-03-03 10:29:00 +0100

    to print a copy of buffer

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memcpy - prints a copy of buffer with set numbers
+ * @dest: variable to set
+ * @src: variable to set dest
+ * @n: lenght of dest to return
+ * Return: char setting
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
