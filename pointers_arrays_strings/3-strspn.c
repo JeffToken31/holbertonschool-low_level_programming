61b2ae1f33f02d57e354332f305fe2910dd474a7
jeff <jeffrey-31@hotmail.fr>
2025-03-03 13:45:05 +0100

    prints number of s = accept

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strspn - prints number of s = accept
+ * @s: variable to set
+ * @accept: char to star of
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+
+	while (s[i] && accept[i])
+	{
+		if (s[i] == accept[i])
+		{
+			return (i + 1);
+		}
+		i++;
+	}
+	return (i + 1);
+}
