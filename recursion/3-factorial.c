a5f30def91b269b37ade799874ee2a419c0b11b5
jeff <jeffrey-31@hotmail.fr>
2025-03-04 15:19:38 +0100

    return a factoriel of n with recursivity

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *factorial - return factoriel of int
+ *@n: int to factor
+ *Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
