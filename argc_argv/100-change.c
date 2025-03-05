071185b1bead75b226b38fd967cecc1b925dc073
jeff <jeffrey-31@hotmail.fr>
2025-03-05 16:26:43 +0100

    print sum of number of cent give

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -20,33 +20,29 @@ int main(__attribute__((unused))int argc, char **argv)
 	{
 		cent = atoi(argv[i]);
 	}
-	while (cent != 0)
+	for (; cent != 0; nbr_cent++)
 	{
 		if (cent >= 25)
 		{
 			cent -= 25;
-			nbr_cent++;
 		}
 		else if (cent >= 10 && cent < 25)
 		{
 			cent -= 10;
-			nbr_cent++;
 		}
 		else if (cent >= 5 && cent < 10)
 		{
 			cent -= 5;
-			nbr_cent++;
 		}
 		else if (cent >= 2 && cent < 5)
 		{
 			cent -= 2;
-			nbr_cent++;
 		}
 		else
 		{
 			cent -= 1;
-			nbr_cent++;
-		}}
+		}
+	}
 	printf("%d\n", nbr_cent);
 	return (0);
 }
