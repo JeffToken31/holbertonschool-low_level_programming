55f720bc616ebe90bc963d72f0a0981385047a37
jeff <jeffrey-31@hotmail.fr>
2025-03-05 12:08:27 +0100

    Print number arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - return is name
+ *@argc: argument counter type int
+ *@argv: strings who's contain arguments
+ *Return: void
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
