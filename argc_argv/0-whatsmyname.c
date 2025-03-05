eb441a01b975ef9eccfded670a02a743257bbaa3
jeff <jeffrey-31@hotmail.fr>
2025-03-05 11:54:59 +0100

    Print is name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - return is name
+ *@argc: argument counter type int
+ *@**argv: strings who's contain arguments
+ *Return: void
+ */
+int main(__attribute__((unused))int argc, char **argv)
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
+
