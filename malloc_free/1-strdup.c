77a45ac259f9f7c4d3e841f526d85c164f55cc23
jeff <jeffrey-31@hotmail.fr>
2025-03-10 10:06:53 +0100

    to duplicate a string with malloc

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ **_strdup - to duplicate a string with malloc
+ *@str: char to duplicate
+ *Return: a string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	char *ptr;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < 2)
+		{
+			return (NULL);
+		}
+		i++;
+	}
+	ptr = malloc(i * sizeof(str));
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+
+	ptr[i] = '\0';
+
+	return (ptr);
+}
