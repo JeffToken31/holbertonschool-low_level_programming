eeac03c381f757bfeddd0808c0cdd4fc24997c06
jeff <jeffrey-31@hotmail.fr>
2025-03-10 10:58:00 +0100

    to concatenate two strings with dynamic allocation

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ **str_concat - to concatenate two strings with dynamic allocation
+ *@s1: char to concatenate
+ *@s2: char to concatenate
+ *Return: a string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *ptr;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc(i + j + 1 * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -56,4 +56,5 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
