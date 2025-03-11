7ec9a9e7cb2eb22f0d0b54935d2513c099954d45
jeff <jeffrey-31@hotmail.fr>
2025-03-11 15:10:17 +0100

    README.md

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct to reference dogs
+ * @name: dog name
+ * @age: age of dog
+ * @owner: name of owner dog
+ *
+ * Description: to reference dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
