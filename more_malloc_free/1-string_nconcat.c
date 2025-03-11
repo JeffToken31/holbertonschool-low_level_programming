adce57daced9adfd89bdbc31423b74bf21089ea3
jeff <jeffrey-31@hotmail.fr>
2025-03-11 10:31:42 +0100

    to concatenate one string more one part of other string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 	{}
 	for (j = 0; j < n; j++)
