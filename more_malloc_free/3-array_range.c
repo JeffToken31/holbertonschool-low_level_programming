0285de56cef87e652c163a2e3150f9e679f6f1cd
jeff <jeffrey-31@hotmail.fr>
2025-03-11 13:47:22 +0100

    to return an array

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < max + 1 - min; i++)
+	for (i = 0; i < max - min + 1; i++)
 		array[i] = i;
 	return (array);
 }
