08394b01be7944904dd09aca7a5094ca16b5cf61
jeff <jeffrey-31@hotmail.fr>
2025-03-11 21:23:59 +0100

    to update a struct

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
-#include <string.h>
 
 /**
  *init_dog - to update a struct
@@ -14,33 +12,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int i, j;
-
 	if (d == NULL)
 		exit(-1);
 	if (name == NULL || owner == NULL)
 		exit(-1);
 
-	for (i = 0; name[i]; i++)
-	{}
-	for (j = 0; owner[j]; j++)
-	{}
-	d->name = malloc(i + 1);
-	if (d->name == NULL)
-		exit(-1);
-	d->owner = malloc(j + 1);
-	if (d->owner == NULL)
-		exit(-1);
-	for (i = 0; name[i]; i++)
-	{
-		d->name[i] = name[i];
-	}
-	i++;
-	d->name[i] = '\0';
-	for (j = 0; owner[j]; j++)
-	{
-		d->owner[j] = owner[j];
-	}
-	d->owner[j] = '\0';
+	d->name = name;
+	d->owner = owner;
 	d->age = age;
 }
