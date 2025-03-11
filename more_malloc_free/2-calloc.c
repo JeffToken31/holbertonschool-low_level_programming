4e7f810b7bd682d60a75616cb8b0f6fef3ed4cd1
jeff <jeffrey-31@hotmail.fr>
2025-03-11 12:21:00 +0100

    to allocate memomry for an array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,13 +12,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *str;
+	int i = 0;
 
 	if (nmemb == 0)
 		return (NULL);
 	if (size == 0)
 		return (NULL);
-	str = calloc(nmemb, sizeof(int));
+	str = malloc(nmemb * size);
 	if (str == NULL)
 		return (NULL);
+	while(str[i])
+		str[i++] = 0;
+
 	return (str);
 }
