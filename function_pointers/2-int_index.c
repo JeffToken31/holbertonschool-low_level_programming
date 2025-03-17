44064fd12c4f4a116d55545db9a58ee3d1e1c273
jeff <jeffrey-31@hotmail.fr>
2025-03-17 11:29:25 +0100

    return location of int if is shearched

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ *int_index - return location of int if is shearched
+ *@array: pointer array
+ *@size: size of array
+ *@cmp: call pointer function
+ *Return: location of int in array
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL)
+		exit(-1);
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
