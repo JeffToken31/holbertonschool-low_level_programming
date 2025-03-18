ed9d9654eef617ad13f38138d7ecd4fda6109b67
jeff <jeffrey-31@hotmail.fr>
2025-03-18 11:02:59 +0100

    print string with handle separator

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *print_strings - print string with handle separator
+ *@separator: to choose separator between number
+ *@n: numbers of arguments to print
+ *Return: print string with separator
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list string;
+	char *copy;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(string, n);
+	for (i = 0; i < n; i++)
+	{
+		copy = va_arg(string, char *);
+
+		if (copy == NULL)
+			copy = "nil";
+		if (i == n - 1)
+			printf("%s", copy);
+		else
+			printf("%s%s", copy, separator);
+	}
+	printf("\n");
+	va_end(string);
+}
