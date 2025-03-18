29b68932713d505ca3d75797c295cc183431840a
jeff <jeffrey-31@hotmail.fr>
2025-03-18 10:48:46 +0100

    print numbers with handle separator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,9 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
-			printf("%d\n", va_arg(numbers, int));
+			printf("%d", va_arg(numbers, int));
 		else
 			printf("%d%s", va_arg(numbers, int), separator);
 	}
+	printf("\n");
 	va_end(numbers);
 }
