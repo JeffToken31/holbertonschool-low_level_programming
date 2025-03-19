5821ce2dbad3d81862530d13389559fe5ff82394
jeff <jeffrey-31@hotmail.fr>
2025-03-19 20:00:51 +0100

    my first asm file,hello word

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,17 @@
+section	.data			;section data qui enregistre les variable
+hello db "Hello World", 0xA	;nom_variable Define Byte 0xA= saut de ligne ascii
+lenght equ $ - hello		;l'assembleur reconné equ et attribut la valeur 
+
+section	.text			;section pour inscrire les intructions
+	global _start		;doit etre déclarer pour le linker
+
+_start:				;equivalent du main en C
+	mov rax, 1		;appel de la fonction voulu 1= sys_write(1,hello,lenght)
+	mov rdi, 1		;parametre 1=sdtout
+	mov rsi, hello		;le message a affiché
+	mov rdx, lenght		;taille du message a print
+	syscall			;appel kernel pour execter sys_write
+
+	mov rax, 60		;60 = sys_exit(retour eventuel)
+	mov rdi, 0		;pour que exit me retourne 0
+	syscall			;lancement de l'instruction
