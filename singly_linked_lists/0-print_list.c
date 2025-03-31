44db17b15c16ffd8cd070d70ff85a2773fe2c50d
jeff <jeffrey-31@hotmail.fr>
2025-03-31 10:07:15 +0200

    preparinting to this project

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+/**
+ *print_list : print a singly linked list
+ *@list: singly linked list to print
+ *@h: numbers elements fo list
+ *Return: numbers of nodes in list
+ */
+
+ size_t print_list(const list_t *h)
+{
+
+	return(size);
+}
