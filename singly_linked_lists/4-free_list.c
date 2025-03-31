23607ad91fc302ea848df8ef3a8a8e236f5a8b78
jeff <jeffrey-31@hotmail.fr>
2025-03-31 19:35:32 +0200

    my output say no leak are possible but not the same output than checker, then i just check the difference

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *free_list - to pop(unallocate) all element of list
+ *@head: pointer of the list
+ *Return: no needed just free element of list and list
+ */
+
+void free_list(list_t *head)
+{
+	
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head -> next;
+	}
+
+}
