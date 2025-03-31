0e19f48b12dd3ef354d1ea3788c7a55774689527
jeff <jeffrey-31@hotmail.fr>
2025-03-31 11:26:35 +0200

    to check if my code is attempting by chevcker

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,12 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
 /**
  *print_list : print a singly linked list
- *@list: singly linked list to print
- *@h: numbers elements fo list
+ *@h: *pointer to elements of list
  *Return: numbers of nodes in list
  */
 
  size_t print_list(const list_t *h)
 {
+	size_t size = 0;
 
-	return(size);
+
+	if (h == NULL)
+	{
+		printf("[0] (nil)");
+		return (size);
+	}
+	while(h != NULL)
+	{
+		size++;
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (size);
 }
