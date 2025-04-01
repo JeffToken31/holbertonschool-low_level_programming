07d16de044e45c2a8c90202772eca2476eb6bb0f
jeff <jeffrey-31@hotmail.fr>
2025-04-01 11:00:29 +0200

    update element->prev before allocate element at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 	element->next = tmp->next;
-	tmp->next = element;
 	element->prev = tmp;
+	tmp->next = element;
 	return (element);
 }
