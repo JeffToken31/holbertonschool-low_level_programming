229fbd2d98124f5a6b5e675ce8792e0f87d35945
jeff <jeffrey-31@hotmail.fr>
2025-04-02 17:16:32 +0200

    just change permissions line32

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	fto = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
