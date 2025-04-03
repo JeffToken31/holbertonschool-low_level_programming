f46b674be34f55d5e0cbd467336ae9364f6e714b
jeff <jeffrey-31@hotmail.fr>
2025-04-03 14:52:18 +0200

    add close line 36

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,11 +22,6 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	ffrom = open(argv[1], O_RDONLY);
-	if (ffrom == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 
 	fto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fto == -1)
@@ -46,7 +41,7 @@ int main(int argc, char **argv)
 			exit(99);
 		}
 	}
-	if (byte_read == -1)
+	if (byte_read == -1 || ffrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(ffrom);
